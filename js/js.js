0faec76f4977c59f1d9388a5f241c6f5e39ad797
muhammedosama8 <61082930+muhammedosama8@users.noreply.github.com>
2020-04-16 00:30:52 +0200

    first commit

--- js/js.js
+++ js/js.js
@@ -1,36 +1,26 @@
-$(function () {
-    'use strict';
-    console.log($(window).scrollTop());
-    $(window).scroll('scroll',function () {
-        if ($(window).scrollTop() > 200){
-            $('.cd-top').fadeIn();
-
-        }else {
-            $('.cd-top').fadeOut();
-        }
-
-    });
-});
 $(document).ready(function () {
-
+    $('.cd-top').fadeOut();
     $(function () {
         $('.carousel').carousel({
             interval: 2000
         });
     });
+    $(function () {
+        'use strict';
 
+        $(window).scroll('scroll',function () {
+            if ($(window).scrollTop() > 200){
+                $('.cd-top').fadeIn();
+
+            }else {
+                $('.cd-top').fadeOut();
+            }
+
+        });
+    });
 
 });
-/*
-document.getElementById("apps").onclick=function (evt) {
-    evt.preventDefault();
-    document.getElementById('appsDropdown').classList.toggle("show");
-    document.getElementById('pagesDropdown').classList.remove("show");
-    document.getElementById('featuresDropdown').classList.remove("show");
-    document.getElementById('baseDropdown').classList.remove("show");
-    document.getElementById('formsDropdown').classList.remove("show");
-    document.getElementById('iconsDropdown').classList.remove("show");
-}; */
+
 $('#apps').click(function (evt) {
     evt.preventDefault();
     $('#appsDropdown').toggle('show');
@@ -66,64 +56,10 @@ $('#icons').click(function (evt) {
     $('#iconsDropdown').toggle('show');
     $('#appsDropdown').removeClass('show')
 });
-/*
-document.getElementById("pages").onclick=function (evt) {
-    evt.preventDefault();
-    document.getElementById('pagesDropdown').classList.toggle("show");
-    document.getElementById('appsDropdown').classList.remove("show");
-    document.getElementById('featuresDropdown').classList.remove("show");
-    document.getElementById('baseDropdown').classList.remove("show");
-    document.getElementById('formsDropdown').classList.remove("show");
-    document.getElementById('iconsDropdown').classList.remove("show");
 
-};
-document.getElementById("features").onclick=function (evt) {
-    evt.preventDefault();
-    document.getElementById('featuresDropdown').classList.toggle("show");
-    document.getElementById('appsDropdown').classList.remove("show");
-    document.getElementById('pagesDropdown').classList.remove("show");
-    document.getElementById('baseDropdown').classList.remove("show");
-    document.getElementById('formsDropdown').classList.remove("show");
-    document.getElementById('iconsDropdown').classList.remove("show");
-};
-document.getElementById("base").onclick=function (evt) {
-    evt.preventDefault();
-    document.getElementById('baseDropdown').classList.toggle("show");
-    document.getElementById('featuresDropdown').classList.remove("show");
-    document.getElementById('appsDropdown').classList.remove("show");
-    document.getElementById('pagesDropdown').classList.remove("show");
-    document.getElementById('formsDropdown').classList.remove("show");
-    document.getElementById('iconsDropdown').classList.remove("show");
-};
-document.getElementById("forms").onclick=function (evt) {
-    evt.preventDefault();
-    document.getElementById('formsDropdown').classList.toggle("show");
-    document.getElementById('featuresDropdown').classList.remove("show");
-    document.getElementById('appsDropdown').classList.remove("show");
-    document.getElementById('pagesDropdown').classList.remove("show");
-    document.getElementById('baseDropdown').classList.remove("show");
-    document.getElementById('iconsDropdown').classList.remove("show");
-};
-document.getElementById("icons").onclick=function (evt) {
-    evt.preventDefault();
-    document.getElementById('iconsDropdown').classList.toggle("show");
-    document.getElementById('formsDropdown').classList.remove("show");
-    document.getElementById('featuresDropdown').classList.remove("show");
-    document.getElementById('appsDropdown').classList.remove("show");
-    document.getElementById('pagesDropdown').classList.remove("show");
-    document.getElementById('baseDropdown').classList.remove("show");
-};
-*/
+
 /* ************************************* */
-function toggle(source) {
-    let checkboxes = document.getElementsByClassName('.input');
-    for(var i=0, n=checkboxes.length;i<n;i++) {
-        checkboxes[i].checked = source.checked;
-    }
-}
 
-$('.fa-ellipsis-h').onclick(function () {
-    $(this).addClass('test');
-});
+
 
 
